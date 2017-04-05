c67deda44445b7b30c21d1f7ac55395b9d956886
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-06 05:26:59 +0530

    Update myvote.js

--- myvote.js
+++ myvote.js
@@ -1,39 +1,41 @@
 $(document).ready(function(){
-	$(".total").click(function(e){
-		//e.stopPropagation();
-		var code = $(this).find('.number').attr('id');
-	$.ajax({
-		type: "GET",
-		url: "https://poll-upvoting.herokuapp.com/getting-count.php",
-		data: ({'txtcode' : code}),
-		success: function(data){	
-		var myvar = data;
-		//alert (myvar);	
-		//location.reload();
-		//$(this).find('.number').html(myvar);
-			}	
-		});
-	});
-
-	var flag=false;
-	$(".toggle").click(function(e){
-		e.stopPropagation();
+	$('.total').click(function() {
 		var costVal = parseInt($(this).find('.number').text(), 10);
-		if(flag)
-		{
+		if($(this).hasClass('toggled') ) {
+			var code = $(this).find('.number').attr('id');
+			// Vote Count Down
+			$.ajax({
+				type: "GET",
+				url: "https://poll-upvoting.herokuapp.com/count-down.php",
+				data: ({'votecode' : code}),
+				success: function(data){	
+					var myvar = data;
+					//alert (myvar);	
+					}	
+				});			
 			$(this).find('.number').text(costVal-1);
 			$(this).removeClass('toggled');
 			$(this).css("background", "#fff");
-			$(this).css("border-color", "#6e6e71");			
+			$(this).css("border-color", "#6e6e71");	 
 		}
 		else
 		{
+			var code = $(this).find('.number').attr('id');
+			// Vote Count Up
+			$.ajax({
+				type: "GET",
+				url: "https://poll-upvoting.herokuapp.com/getting-count.php",
+				data: ({'txtcode' : code}),
+				success: function(data){	
+					var myvar = data;
+					//alert (myvar);	
+					}	
+				});			
 			$(this).find('.number').text(costVal+1);
 			$(this).addClass('toggled');
 			$(this).css("background", "#f4511c");
 			$(this).css("border-color", "#f4511c");
-		}
-		   flag=!flag;
+			$(this).find(.toggled .number).css("color", "#fff");
+		}	
 	});	
-	
-});
+});	
