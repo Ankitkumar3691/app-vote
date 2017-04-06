0119e48f2eb02575f396e32170dc17b2d1588fb4
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-07 04:16:49 +0530

    Update show-poll.php

--- admin/show-poll.php
+++ admin/show-poll.php
@@ -76,17 +76,10 @@ include ('config.php');
 $sql = 'SELECT * from vote ORDER BY vote."Id"';
 $result = pg_query($sql) or die('Query failed: ' . pg_last_error());
 
-//if ($result-> pg_num_rows > 0) {
-    // output data of each row
-    //while($row = $result-> pg_fetch_assoc()) {
 	while ($row = pg_fetch_array($result)) {	
 		$id= $row["Id"];
-        echo "<tr><td>" . $row["Id"]. "</td><td>" . $row["Question"]. "</td><td>" . $row["Count_num"]. "</td><td>" . $row["Question_Desc"] . "</td><td>" ."<a href='#'>Edit</a>"  ."&nbsp;/&nbsp;<a href='#'>Delete</a>"  ."</td></tr>";
+        echo "<tr><td>" . $row["Id"]. "</td><td>" . $row["Question"]. "</td><td>" . $row["Count_num"]. "</td><td>" . $row["Quest_Desc"] . "</td><td>" ."<a href='#'>Edit</a>"  ."&nbsp;/&nbsp;<a href='#'>Delete</a>"  ."</td></tr>";
     }
-//} 
-//else {
-//    echo "0 results";
-//}
 ?>
 		</tbody>
 		</table>	
