9f2c8e180e986dda86868b4a727bd52f5d99dd83
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-07 03:31:57 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -15,12 +15,15 @@ if (isset ($_POST['submit'])) {
 		$password= pg_escape_string($_POST['password']);
 		
 		// Connecting, selecting database
-		include 'config.php';
+	$connection = pg_connect("host=ec2-23-23-223-2.compute-1.amazonaws.com dbname=d1filjgshltm5 user=mtracxsevywyhp password=d1950f8ce89de40987a180f56de2bc99250da5810e346b57e6bdf5e957c18c3c")
+    or die('Could not connect: ' . pg_last_error());
 			
 		// SQL query to fetch information of registerd users and finds user match.
 	$query = 'Select * from admin_user where admin_user."Username" = \''.$username.'\' and admin_user."Password" = \''.$password.'\'';
 	
-		$rows = pg_num_rows($query);
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	
+		$rows = pg_num_rows($result);
 		if ($rows == 1) {
 			$_SESSION['login_user']=$username; // Initializing Session
 			header("location: profile.php"); // Redirecting To Other Page
@@ -28,6 +31,9 @@ if (isset ($_POST['submit'])) {
 		else {
 			$error = "Username or Password is invalid";
 		}
+		// Free resultset
+		pg_free_result($result);
+		
 		// Closing connection
 		pg_close($connection);
 	}
