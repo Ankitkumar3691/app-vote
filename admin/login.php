d9f46a89ebf3904e6125fa7c769fecc5ba8d2a5b
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-07 03:53:27 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -15,15 +15,12 @@ if (isset ($_POST['submit'])) {
 		$password= pg_escape_string($_POST['password']);
 		
 		// Connecting, selecting database
-	//$connection = pg_connect("host=ec2-23-23-223-2.compute-1.amazonaws.com dbname=d1filjgshltm5 user=mtracxsevywyhp password=d1950f8ce89de40987a180f56de2bc99250da5810e346b57e6bdf5e957c18c3c")
-    //or die('Could not connect: ' . pg_last_error());
-			
-	include 'config.php';	
+		include 'config.php';	
 	
 		// SQL query to fetch information of registerd users and finds user match.
-	$query = 'Select * from admin_user where admin_user."Username" = \''.$username.'\' and admin_user."Password" = \''.$password.'\'';
+		$query = 'Select * from admin_user where admin_user."Username" = \''.$username.'\' and admin_user."Password" = \''.$password.'\'';
 	
-	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 	
 		$rows = pg_num_rows($result);
 		if ($rows == 1) {
