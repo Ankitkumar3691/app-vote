b2f7685648d15c59482dc81b74aeab0cab506615
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-08 01:43:37 +0530

    Update profile.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/profile.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/profile.php
@@ -29,8 +29,8 @@ include ('config.php');
 <link href="css/colors/blue.css" id="theme"  rel="stylesheet">
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-		<script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css">
+		<script src="js/jquery.dataTables.min.js"></script>
+		<link href="css/jquery.dataTables.min.css" rel="stylesheet">
 		<script>
 		$(document).ready(function() {
 			$('#example').DataTable( {
