972fcc0b6c85d1224a73daa60f3a92e7c5f63154
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-08 00:03:35 +0530

    Update profile.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/profile.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/profile.php
@@ -62,13 +62,13 @@
         <!-- /.dropdown -->
         <li class="dropdown"> <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="default_avatar.png" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">admin</b> </a>
           <ul class="dropdown-menu dropdown-user animated flipInY">
-            <li><a href="#"><i class="ti-user"></i> My Profile</a></li>
+            <!--li><a href="#"><i class="ti-user"></i> My Profile</a></li>
             <li><a href="#"><i class="ti-wallet"></i> My Balance</a></li>
             <li><a href="#"><i class="ti-email"></i> Inbox</a></li>
             <li role="separator" class="divider"></li>
             <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#"><i class="fa fa-power-off"></i> Logout</a></li>
+            <li role="separator" class="divider"></li-->
+            <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
           </ul>
           <!-- /.dropdown-user -->
         </li>
