621851398b970b234fb570ff1372c015b13def81
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-08 00:12:16 +0530

    Update index.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/index.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/index.php
@@ -22,12 +22,6 @@ if(isset($_SESSION['login_user'])){
 <link href="css/style.css" rel="stylesheet">
 <!-- color CSS -->
 <link href="css/colors/blue.css" id="theme"  rel="stylesheet">
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
 <script src="http://www.w3schools.com/lib/w3data.js"></script>
 </head>
 <body>
@@ -50,14 +44,6 @@ if(isset($_SESSION['login_user'])){
             <input class="form-control" type="password" name="password" required="" placeholder="Password">
           </div>
         </div>
-        <!--div class="form-group">
-          <div class="col-md-12">
-            <div class="checkbox checkbox-primary pull-left p-t-0">
-              <input id="checkbox-signup" type="checkbox">
-              <label for="checkbox-signup"> Remember me </label>
-            </div>
-            <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
-        </div-->
         <div class="form-group text-center m-t-20">
           <div class="col-xs-12">
             <!--button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Log In</button-->
