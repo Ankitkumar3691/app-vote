45410871bcd4ef68371fc6a0fed96fcec7670fcd
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-07 23:36:27 +0530

    Add files via upload

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/index.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/index.php
@@ -1,3 +1,9 @@
+<?php
+include('login.php'); // Includes Login Script
+if(isset($_SESSION['login_user'])){
+	header("location: profile.php");
+}
+?>
 <!DOCTYPE html>  
 <html lang="en">
 <head>
@@ -7,15 +13,9 @@
 <meta name="description" content="">
 <meta name="author" content="">
 <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
-<title>Poll Voting Interface</title>
+<title>Poll Admin</title>
 <!-- Bootstrap Core CSS -->
 <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- Menu CSS -->
-<link href="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
-<!-- toast CSS -->
-<link href="../plugins/bower_components/toast-master/css/jquery.toast.css" rel="stylesheet">
-<!-- morris CSS -->
-<link href="../plugins/bower_components/morrisjs/morris.css" rel="stylesheet">
 <!-- animation CSS -->
 <link href="css/animate.css" rel="stylesheet">
 <!-- Custom CSS -->
@@ -28,185 +28,86 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-19175540-9', 'auto');
-  ga('send', 'pageview');
-
-</script>
+<script src="http://www.w3schools.com/lib/w3data.js"></script>
 </head>
 <body>
 <!-- Preloader -->
 <div class="preloader">
   <div class="cssload-speeding-wheel"></div>
 </div>
-<div id="wrapper">
-  <!-- Navigation -->
-  <nav class="navbar navbar-default navbar-static-top m-b-0">
-    <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
-      <div class="top-left-part"><a class="logo" href="index.html"><b><img src="../plugins/images/eliteadmin-logo.png" alt="home" /></b><span class="hidden-xs"><img src="../plugins/images/eliteadmin-text.png" alt="home" /></span></a></div>
-      <ul class="nav navbar-top-links navbar-left hidden-xs">
-        <li><a href="javascript:void(0)" class="open-close hidden-xs waves-effect waves-light"><i class="icon-arrow-left-circle ti-menu"></i></a></li>
-        <li>
-          <form role="search" class="app-search hidden-xs">
-            <input type="text" placeholder="Search..." class="form-control">
-            <a href=""><i class="fa fa-search"></i></a>
-          </form>
-        </li>
-      </ul>
-      <ul class="nav navbar-top-links navbar-right pull-right">  
-        <!-- /.dropdown -->
-        <li class="dropdown"> <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="default_avatar.png" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">admin</b> </a>
-          <ul class="dropdown-menu dropdown-user animated flipInY">
-            <li><a href="#"><i class="ti-user"></i> My Profile</a></li>
-            <li><a href="#"><i class="ti-wallet"></i> My Balance</a></li>
-            <li><a href="#"><i class="ti-email"></i> Inbox</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#"><i class="fa fa-power-off"></i> Logout</a></li>
-          </ul>
-          <!-- /.dropdown-user -->
-        </li>
-        <li class="right-side-toggle"> <a class="waves-effect waves-light" href="javascript:void(0)"><i class="ti-settings"></i></a></li>
-        <!-- /.dropdown -->
-      </ul>
-    </div>
-    <!-- /.navbar-header -->
-    <!-- /.navbar-top-links -->
-    <!-- /.navbar-static-side -->
-  </nav>
-  <!-- Left navbar-header -->
-  <div class="navbar-default sidebar" role="navigation">
-    <div class="sidebar-nav navbar-collapse slimscrollsidebar">
-      <ul class="nav" id="side-menu">
-        <li class="sidebar-search hidden-sm hidden-md hidden-lg">
-          <!-- input-group -->
-          <div class="input-group custom-search-form">
-            <input type="text" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-            <button class="btn btn-default" type="button"> <i class="fa fa-search"></i> </button>
-            </span> </div>
-          <!-- /input-group -->
-        </li>
-        <li> <a href="index.html" class="waves-effect active"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Dashboard <span class="fa arrow"></span> </span></a>
-        </li>
-<li> <a href="#" class="waves-effect"><i data-icon="7" class="linea-icon linea-basic fa-fw "></i> <span class="hide-menu ">Menu 1<span class="fa arrow"></span></span></a>
-</li>
-        <li><a href="#" class="waves-effect"><i data-icon=")" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Menu 2<span class="fa arrow"></span></span></a>
-        </li>
-        <li> <a href="#" class="waves-effect"><i data-icon="/" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Menu 3<span class="fa arrow"></span></span></a> 
-        </li>
-        <li> <a href="#" class="waves-effect"><i data-icon="&#xe00b;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Menu 4<span class="fa arrow"></span></span></a>
-        </li>
-        <li> <a href="#" class="waves-effect"><i data-icon="O" class="linea-icon linea-software fa-fw"></i> <span class="hide-menu">Menu 5<span class="fa arrow"></span></span></a>
-        </li>
-        <li> <a href="#" class="waves-effect"><i data-icon="&#xe008;" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Menu 6<span class="fa arrow"></span></span></a>  
-        </li>
-        <li> <a href="#" class="waves-effect"><i data-icon="P" class="linea-icon linea-basic fa-fw"></i> <span class="hide-menu">Menu 7</span></a> </li>
-        <li> <a href="javascript:void(0)" class="waves-effect"><i data-icon="F" class="linea-icon linea-software fa-fw"></i> <span class="hide-menu">Menu 8<span class="fa arrow"></span></span></a>
-        </li>
-        
-</ul>
-    </div>
-  </div>
-  <!-- Left navbar-header end -->
-  <!-- Page Content -->
-  <div id="page-wrapper">
-    <div class="container-fluid">
-      <div class="row bg-title">
-        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-          <h4 class="page-title">WELCOME ADMIN</h4>
-        </div>
-		
-        <!-- /.col-lg-12 -->
-      <div class="right-sidebar">
-        <div class="slimscrollright">
-          <div class="rpanel-title"> Service Panel <span><i class="ti-close right-side-toggle"></i></span> </div>
-          <div class="r-panel-body">
-            <ul>
-              <li><b>Layout Options</b></li>
-              
-              <li>
-                <div class="checkbox checkbox-info">
-                  <input id="checkbox1" type="checkbox" class="fxhdr">
-                  <label for="checkbox1"> Fix Header </label>
-                </div>
-              </li>
-            </ul>
-            <ul id="themecolors" class="m-t-20">
-              <li><b>With Light sidebar</b></li>
-              <li><a href="javascript:void(0)" theme="default" class="default-theme">1</a></li>
-              <li><a href="javascript:void(0)" theme="green" class="green-theme">2</a></li>
-              <li><a href="javascript:void(0)" theme="gray" class="yellow-theme">3</a></li>
-              <li><a href="javascript:void(0)" theme="blue" class="blue-theme working">4</a></li>
-              <li><a href="javascript:void(0)" theme="purple" class="purple-theme">5</a></li>
-              <li><a href="javascript:void(0)" theme="megna" class="megna-theme">6</a></li>
-              <li><b>With Dark sidebar</b></li>
-              <br>
-              <li><a href="javascript:void(0)" theme="default-dark" class="default-dark-theme">7</a></li>
-              <li><a href="javascript:void(0)" theme="green-dark" class="green-dark-theme">8</a></li>
-              <li><a href="javascript:void(0)" theme="gray-dark" class="yellow-dark-theme">9</a></li>
-
-              <li><a href="javascript:void(0)" theme="blue-dark" class="blue-dark-theme">10</a></li>
-              <li><a href="javascript:void(0)" theme="purple-dark" class="purple-dark-theme">11</a></li>
-              <li><a href="javascript:void(0)" theme="megna-dark" class="megna-dark-theme">12</a></li>
-            </ul>
+<section id="wrapper" class="login-register">
+  <div class="login-box">
+    <div class="white-box">
+      <form class="form-horizontal form-material" id="loginform" action="" method="post">
+        <h3 class="box-title m-b-20">Sign In</h3>
+        <div class="form-group ">
+          <div class="col-xs-12">
+            <input class="form-control" type="text" name="username" required="" placeholder="Username">
           </div>
-        </div><div class="slimScrollBar" style="background: rgb(220, 220, 220); width: 5px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 233.38px;"></div><div class="slimScrollRail" style="width: 5px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
-      </div>
-		
-		
-      </div>
-   
-    <footer class="footer text-center"> 2017 &copy; Poll Voting Admin Interface </footer>
+        </div>
+        <div class="form-group">
+          <div class="col-xs-12">
+            <input class="form-control" type="password" name="password" required="" placeholder="Password">
+          </div>
+        </div>
+        <!--div class="form-group">
+          <div class="col-md-12">
+            <div class="checkbox checkbox-primary pull-left p-t-0">
+              <input id="checkbox-signup" type="checkbox">
+              <label for="checkbox-signup"> Remember me </label>
+            </div>
+            <a href="javascript:void(0)" id="to-recover" class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i> Forgot pwd?</a> </div>
+        </div-->
+        <div class="form-group text-center m-t-20">
+          <div class="col-xs-12">
+            <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Log In</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 m-t-10 text-center">
+            <div class="social"><a href="javascript:void(0)" class="btn  btn-facebook" data-toggle="tooltip"  title="Login with Facebook"> <i aria-hidden="true" class="fa fa-facebook"></i> </a> <a href="javascript:void(0)" class="btn btn-googleplus" data-toggle="tooltip"  title="Login with Google"> <i aria-hidden="true" class="fa fa-google-plus"></i> </a> </div>
+          </div>
+        </div>
+        <div class="form-group m-b-0">
+          <div class="col-sm-12 text-center">
+            <p>Don't have an account? <a href="#" class="text-primary m-l-5"><b>Sign Up</b></a></p>
+          </div>
+        </div>
+      </form>
+      <form class="form-horizontal" id="recoverform" action="index.html">
+        <div class="form-group ">
+          <div class="col-xs-12">
+            <h3>Recover Password</h3>
+            <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
+          </div>
+        </div>
+        <div class="form-group ">
+          <div class="col-xs-12">
+            <input class="form-control" type="text" required="" placeholder="Email">
+          </div>
+        </div>
+        <div class="form-group text-center m-t-20">
+          <div class="col-xs-12">
+            <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
-  <!-- /#page-wrapper -->
-</div>
-</div>
-<!-- /#wrapper -->
+</section>
 <!-- jQuery -->
 <script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap Core JavaScript -->
 <script src="bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Menu Plugin JavaScript -->
 <script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+
 <!--slimscroll JavaScript -->
 <script src="js/jquery.slimscroll.js"></script>
 <!--Wave Effects -->
 <script src="js/waves.js"></script>
-<!--Counter js -->
-<script src="../plugins/bower_components/waypoints/lib/jquery.waypoints.js"></script>
-<script src="../plugins/bower_components/counterup/jquery.counterup.min.js"></script>
-<!--Morris JavaScript -->
-<script src="../plugins/bower_components/raphael/raphael-min.js"></script>
-<script src="../plugins/bower_components/morrisjs/morris.js"></script>
 <!-- Custom Theme JavaScript -->
 <script src="js/custom.min.js"></script>
-<script src="js/dashboard1.js"></script>
-<!-- Sparkline chart JavaScript -->
-<script src="../plugins/bower_components/jquery-sparkline/jquery.sparkline.min.js"></script>
-<script src="../plugins/bower_components/jquery-sparkline/jquery.charts-sparkline.js"></script>
-<script src="../plugins/bower_components/toast-master/js/jquery.toast.js"></script>
-<!-- <script type="text/javascript">
-  
-   $(document).ready(function() {
-      $.toast({
-        heading: 'Welcome to Elite admin',
-        text: 'Use the predefined ones, or specify a custom position object.',
-        position: 'top-right',
-        loaderBg:'#ff6849',
-        icon: 'info',
-        hideAfter: 3500, 
-        
-        stack: 6
-      })
-    });
-</script> -->
 <!--Style Switcher -->
 <script src="../plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
 </body>
