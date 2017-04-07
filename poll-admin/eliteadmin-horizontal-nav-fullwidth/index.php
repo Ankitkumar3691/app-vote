b755e9541830e312a95d38df40cce52dfa281751
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-07 23:54:47 +0530

    Update index.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/index.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/index.php
@@ -14,20 +14,10 @@ if(isset($_SESSION['login_user'])){
 <meta name="author" content="">
 <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
 <title>Poll Admin</title>
-<!-- Bootstrap Core CSS -->
-<link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- animation CSS -->
-<link href="css/animate.css" rel="stylesheet">
 <!-- Custom CSS -->
 <link href="css/style.css" rel="stylesheet">
 <!-- color CSS -->
 <link href="css/colors/blue.css" id="theme"  rel="stylesheet">
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
 <script src="http://www.w3schools.com/lib/w3data.js"></script>
 </head>
 <body>
@@ -37,7 +27,6 @@ if(isset($_SESSION['login_user'])){
 </div>
 <section id="wrapper" class="login-register">
   <div class="login-box">
-    <div class="white-box">
 	<div id="main">
 		<div id="login">
 			<h2>Login Management</h2>
@@ -50,41 +39,7 @@ if(isset($_SESSION['login_user'])){
 			</form>
 		</div>
 	</div>
-      <form class="form-horizontal" id="recoverform" action="index.html">
-        <div class="form-group ">
-          <div class="col-xs-12">
-            <h3>Recover Password</h3>
-            <p class="text-muted">Enter your Email and instructions will be sent to you! </p>
-          </div>
-        </div>
-        <div class="form-group ">
-          <div class="col-xs-12">
-            <input class="form-control" type="text" required="" placeholder="Email">
-          </div>
-        </div>
-        <div class="form-group text-center m-t-20">
-          <div class="col-xs-12">
-            <button class="btn btn-primary btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Reset</button>
-          </div>
-        </div>
-      </form>
-    </div>
   </div>
 </section>
-<!-- jQuery -->
-<script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap Core JavaScript -->
-<script src="bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Menu Plugin JavaScript -->
-<script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
-
-<!--slimscroll JavaScript -->
-<script src="js/jquery.slimscroll.js"></script>
-<!--Wave Effects -->
-<script src="js/waves.js"></script>
-<!-- Custom Theme JavaScript -->
-<script src="js/custom.min.js"></script>
-<!--Style Switcher -->
-<script src="../plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
 </body>
 </html>
