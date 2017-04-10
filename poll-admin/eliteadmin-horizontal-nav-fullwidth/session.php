95010030d7997ffb2a227bfd5e2f29fa8a8c478c
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-10 22:45:51 +0530

    Update session.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/session.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/session.php
@@ -10,16 +10,18 @@ session_start();
 $user_check= $_SESSION['login_user'];
 
 // SQL Query To Fetch Complete Information Of User
+//$ses_sql=mysql_query("select username from admin_user where Username='$user_check'", $connection);
+//$row = mysql_fetch_assoc($ses_sql);
+
 $query = 'SELECT admin_user."Username" from admin_user where admin_user."Username" = \''.$user_check.'\'';
 
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-$rows = pg_num_rows($result);
+$row = pg_fetch_assoc($result);
 
-$login_session =$row['username'];
+$login_session = $row['username'];
 
 if(!isset($login_session)){
-	mysql_close($connection); // Closing Connection
 	header('Location: index.php'); // Redirecting To Home Page
 }
 ?>
