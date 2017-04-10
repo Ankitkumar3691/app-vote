987447821d5de012efa0a012905fbcf5f0716b34
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-11 02:52:19 +0530

    Update update-poll.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/update-poll.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/update-poll.php
@@ -74,60 +74,11 @@ if(isset($_GET['id']))
   <div class="cssload-speeding-wheel"></div>
 </div>
 <div id="wrapper">
-  <!-- Navigation -->
-  <nav class="navbar navbar-default navbar-static-top m-b-0">
-    <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
-      <div class="top-left-part"><a class="logo" href="profile.php"><b><img src="../plugins/images/eliteadmin-logo.png" alt="home" /></b><span class="hidden-xs"><img src="../plugins/images/eliteadmin-text.png" alt="home" /></span></a></div>
-      <ul class="nav navbar-top-links navbar-left hidden-xs">
-        <li><a href="javascript:void(0)" class="open-close hidden-xs waves-effect waves-light"><i class="icon-arrow-left-circle ti-menu"></i></a></li>
-        <li>
-          <form role="search" class="app-search hidden-xs">
-            <input type="text" placeholder="Search..." class="form-control">
-            <a href=""><i class="fa fa-search"></i></a>
-          </form>
-        </li>
-      </ul>
-      <ul class="nav navbar-top-links navbar-right pull-right">  
-        <!-- /.dropdown -->
-        <li class="dropdown"> <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="default_avatar.png" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">admin</b> </a>
-          <ul class="dropdown-menu dropdown-user animated flipInY">
-            <!--li><a href="#"><i class="ti-user"></i> My Profile</a></li>
-            <li><a href="#"><i class="ti-wallet"></i> My Balance</a></li>
-            <li><a href="#"><i class="ti-email"></i> Inbox</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#"><i class="ti-settings"></i> Account Setting</a></li>
-            <li role="separator" class="divider"></li-->
-            <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
-          </ul>
-          <!-- /.dropdown-user -->
-        </li>
-        <li class="right-side-toggle"> <a class="waves-effect waves-light" href="javascript:void(0)"><i class="ti-settings"></i></a></li>
-        <!-- /.dropdown -->
-      </ul>
-    </div>
-    <!-- /.navbar-header -->
-    <!-- /.navbar-top-links -->
-    <!-- /.navbar-static-side -->
-  </nav>
-  <!-- Left navbar-header -->
-  <div class="navbar-default sidebar" role="navigation">
-    <div class="sidebar-nav navbar-collapse slimscrollsidebar">
-      <ul class="nav" id="side-menu">
-        <li class="sidebar-search hidden-sm hidden-md hidden-lg">
-          <!-- input-group -->
-          <div class="input-group custom-search-form">
-            <input type="text" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-            <button class="btn btn-default" type="button"> <i class="fa fa-search"></i> </button>
-            </span> </div>
-          <!-- /input-group -->
-        </li>
-        <li> <a href="#" class="waves-effect "><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Poll <span class="fa arrow"></span> </span></a></li>
-        <li> <a href="#" class="waves-effect "><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Settings <span class="fa arrow"></span> </span></a></li>
-</ul>
-    </div>
-  </div>
-  <!-- Left navbar-header end -->
+<?php
+include('nav.php');
+
+include('left-sidebar.php');
+?>
   <!-- Page Content -->
   <div id="page-wrapper">
     <div class="container-fluid">
@@ -145,42 +96,8 @@ if(isset($_GET['id']))
 				<input type="submit" name="Submit" value="Update"/> 
 			</form> 
 		</div>
-				
-        <!-- /.col-lg-12 -->
-      <div class="right-sidebar">
-        <div class="slimscrollright">
-          <div class="rpanel-title"> Service Panel <span><i class="ti-close right-side-toggle"></i></span> </div>
-          <div class="r-panel-body">
-            <ul>
-              <li><b>Layout Options</b></li>
-              
-              <li>
-                <div class="checkbox checkbox-info">
-                  <input id="checkbox1" type="checkbox" class="fxhdr">
-                  <label for="checkbox1"> Fix Header </label>
-                </div>
-              </li>
-            </ul>
-            <ul id="themecolors" class="m-t-20">
-              <li><b>With Light sidebar</b></li>
-              <li><a href="javascript:void(0)" theme="default" class="default-theme">1</a></li>
-              <li><a href="javascript:void(0)" theme="green" class="green-theme">2</a></li>
-              <li><a href="javascript:void(0)" theme="gray" class="yellow-theme">3</a></li>
-              <li><a href="javascript:void(0)" theme="blue" class="blue-theme working">4</a></li>
-              <li><a href="javascript:void(0)" theme="purple" class="purple-theme">5</a></li>
-              <li><a href="javascript:void(0)" theme="megna" class="megna-theme">6</a></li>
-              <li><b>With Dark sidebar</b></li>
-              <br>
-              <li><a href="javascript:void(0)" theme="default-dark" class="default-dark-theme">7</a></li>
-              <li><a href="javascript:void(0)" theme="green-dark" class="green-dark-theme">8</a></li>
-              <li><a href="javascript:void(0)" theme="gray-dark" class="yellow-dark-theme">9</a></li>
-
-              <li><a href="javascript:void(0)" theme="blue-dark" class="blue-dark-theme">10</a></li>
-              <li><a href="javascript:void(0)" theme="purple-dark" class="purple-dark-theme">11</a></li>
-              <li><a href="javascript:void(0)" theme="megna-dark" class="megna-dark-theme">12</a></li>
-            </ul>
-          </div>
-        </div><div class="slimScrollBar" style="background: rgb(220, 220, 220); width: 5px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 233.38px;"></div><div class="slimScrollRail" style="width: 5px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
+<?php include('right-sidebar.php');?>				
+       
       </div>
 		
 		
