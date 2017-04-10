311f58c257eedcc60ca62d2d6afa57970c480009
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-11 02:18:52 +0530

    Update profile.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/profile.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/profile.php
@@ -35,53 +35,10 @@ include ('config.php');
 </div>
 <div id="wrapper">
   <!-- Navigation -->
-  <nav class="navbar navbar-default navbar-static-top m-b-0">
-    <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
-      <div class="top-left-part"><a class="logo" href="profile.php"><b><img src="../plugins/images/eliteadmin-logo.png" alt="home" /></b><span class="hidden-xs"><img src="../plugins/images/eliteadmin-text.png" alt="home" /></span></a></div>
-      <ul class="nav navbar-top-links navbar-left hidden-xs">
-        <li><a href="javascript:void(0)" class="open-close hidden-xs waves-effect waves-light"><i class="icon-arrow-left-circle ti-menu"></i></a></li>
-        <li>
-          <form role="search" class="app-search hidden-xs">
-            <input type="text" placeholder="Search..." class="form-control">
-            <a href=""><i class="fa fa-search"></i></a>
-          </form>
-        </li>
-      </ul>
-      <ul class="nav navbar-top-links navbar-right pull-right">  
-        <!-- /.dropdown -->
-        <li class="dropdown"> <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="default_avatar.png" alt="user-img" width="36" class="img-circle"><b class="hidden-xs">admin</b> </a>
-          <ul class="dropdown-menu dropdown-user animated flipInY">
-            <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
-          </ul>
-          <!-- /.dropdown-user -->
-        </li>
-        <li class="right-side-toggle"> <a class="waves-effect waves-light" href="javascript:void(0)"><i class="ti-settings"></i></a></li>
-        <!-- /.dropdown -->
-      </ul>
-    </div>
-    <!-- /.navbar-header -->
-    <!-- /.navbar-top-links -->
-    <!-- /.navbar-static-side -->
-  </nav>
-  <!-- Left navbar-header -->
-  <div class="navbar-default sidebar" role="navigation">
-    <div class="sidebar-nav navbar-collapse slimscrollsidebar">
-		<ul class="nav" id="side-menu">
-        <li class="sidebar-search hidden-sm hidden-md hidden-lg">
-          <!-- input-group -->
-          <div class="input-group custom-search-form">
-            <input type="text" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-            <button class="btn btn-default" type="button"> <i class="fa fa-search"></i> </button>
-            </span> </div>
-          <!-- /input-group -->
-        </li>
-        <li> <a href="#" class="waves-effect "><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Poll <span class="fa arrow"></span> </span></a></li>
-        <li> <a href="#" class="waves-effect "><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Settings <span class="fa arrow"></span> </span></a></li>
-		</ul>
-    </div>
-  </div>
-  <!-- Left navbar-header end -->
+<?php
+include('nav.php');
+include('left-sidebar.php');
+?>
   <!-- Page Content -->
   <div id="page-wrapper">
     <div class="container-fluid">
@@ -89,9 +46,7 @@ include ('config.php');
         <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
           <h4 class="page-title"></h4>
         </div>
-		
-		
-			
+	
         <!-- /.col-lg-12 -->
       <div class="right-sidebar">
         <div class="slimscrollright">
