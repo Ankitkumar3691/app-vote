7cf1cc6fa2e2483edd650c45ce4571cbb443dc87
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-11 02:18:19 +0530

    Add files via upload

---
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/left-sidebar.php
@@ -0,0 +1,19 @@
+<!-- Left navbar-header -->
+  <div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse slimscrollsidebar">
+		<ul class="nav" id="side-menu">
+        <li class="sidebar-search hidden-sm hidden-md hidden-lg">
+          <!-- input-group -->
+          <div class="input-group custom-search-form">
+            <input type="text" class="form-control" placeholder="Search...">
+            <span class="input-group-btn">
+            <button class="btn btn-default" type="button"> <i class="fa fa-search"></i> </button>
+            </span> </div>
+          <!-- /input-group -->
+        </li>
+        <li> <a href="#" class="waves-effect "><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Poll <span class="fa arrow"></span> </span></a></li>
+        <li> <a href="#" class="waves-effect "><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Settings <span class="fa arrow"></span> </span></a></li>
+		</ul>
+    </div>
+  </div>
+  <!-- Left navbar-header end -->
