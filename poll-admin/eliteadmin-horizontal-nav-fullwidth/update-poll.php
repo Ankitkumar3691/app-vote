1b6799856f5ff9b7596942cd5630fc5cff56dffa
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-11 04:33:44 +0530

    Update update-poll.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/update-poll.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/update-poll.php
@@ -84,7 +84,7 @@ include('left-sidebar.php');
     <div class="container-fluid">
       <div class="row bg-title">
         <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-          <h4 class="page-title">Create New Questions for Poll</h4>
+          <h4 class="page-title">Edit <? echo $a ?> Poll Item</h4>
         </div>
 		
 		<div class="code_update">
