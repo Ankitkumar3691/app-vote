22c6fc02677436b643cd15ddc606acdb5486e704
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-11 23:34:04 +0530

    Update register-data.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/register-data.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/register-data.php
@@ -10,6 +10,16 @@ if (isset ($_POST['submit'])) {
 	// Connecting, selecting database
 	include 'config.php';	
 
+	$select = 'Select * from admin_user where admin_user."Username" = \''.$new_user.'\'';
+	
+	$result = pg_query($select) or die('Query failed: ' . pg_last_error());
+	
+	if ($row = pg_num_rows($result) == 1)
+	{	
+		$message = "This Username is already exist ";
+		echo "<script type='text/javascript'>alert('$message');</script>";
+	}
+	else {	
 	// SQL query to fetch information of registerd users and finds user match.
 	$sql = 'INSERT INTO admin_user ("Username", "Password", "Email", "Name") VALUES (\''.$new_user.'\',\''.$new_pass.'\', \''.$new_email.'\', \''.$new_name.'\')';
 	
@@ -21,6 +31,7 @@ if (isset ($_POST['submit'])) {
 		echo 'alert("Registration Successfully");'; 
 		echo 'window.location.href = "http://app.upvoteapp.com/poll-admin/eliteadmin-horizontal-nav-fullwidth/";';
 		echo '</script>';	
+	}
 	}
 	// Free resultset
 	pg_free_result($result);
