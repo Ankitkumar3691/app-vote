8c7cfdabdb580bf3c0ca3c94cf6f9baecdaeddc4
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-14 00:19:40 +0530

    Update poll-with-settings.php

--- poll-with-settings.php
+++ poll-with-settings.php
@@ -11,33 +11,13 @@
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 main">
 		<div class="container">
 			<!-- Show Logo -->
-			<div id="show-image">
-			<?php 
-			
-			// Connecting, selecting database
-			$dbconn = pg_connect("host=ec2-23-23-223-2.compute-1.amazonaws.com dbname=d1filjgshltm5 user=mtracxsevywyhp password=d1950f8ce89de40987a180f56de2bc99250da5810e346b57e6bdf5e957c18c3c")
-				or die('Could not connect: ' . pg_last_error());
-			
-				$sql = 'SELECT * from poll_setting where poll_setting."poll_id" = "1"';
-				
-				$sql_result = pg_query($sql) or die('Query failed: ' . pg_last_error());
-				
-				while ($row = pg_fetch_array($sql_result)) {	
-					$image_path=$row["logo_path"];	
-					echo "<img src=".$image_path." width=100 height=100/>";		
-				}			
-			?>
-			</div>		
-		
 			<div class="heading">
 				<h1>Vote on Upcoming Features</h1>
 			</div>
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 content_main">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 content">
 <?php
-// Connecting, selecting database
-$dbconn = pg_connect("host=ec2-23-23-223-2.compute-1.amazonaws.com dbname=d1filjgshltm5 user=mtracxsevywyhp password=d1950f8ce89de40987a180f56de2bc99250da5810e346b57e6bdf5e957c18c3c")
-    or die('Could not connect: ' . pg_last_error());
+include 'config.php';
 	
 $query = 'SELECT * from vote ORDER BY vote."Id"';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());	
