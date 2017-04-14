6562fd1c5e13a5611938e171797b176fbd36d230
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-15 05:08:40 +0530

    Update poll-setting.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/poll-setting.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/poll-setting.php
@@ -160,11 +160,12 @@ include('left-sidebar.php');
 						<option value="Beta"><b>Beta</b></option>
 						<option value="Completed"><b>Completed</b></option>
 					</select> </p>				
-				<input id="setting_submit" type="submit" name="submit" value="Save">
+				
 				</center>
 			
 			</div>
 			</div>
+			<input id="setting_submit" type="submit" name="submit" value="Save">
 			</form>
 			
 			</div>
