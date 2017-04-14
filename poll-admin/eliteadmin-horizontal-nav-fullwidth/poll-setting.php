b782f8b9c5c1b572052756ab7fb1c0d316eef7ba
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-14 22:38:06 +0530

    Update poll-setting.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/poll-setting.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/poll-setting.php
@@ -8,35 +8,39 @@ if(isset($_POST['submit'])){
 	$poll_id = pg_escape_string($_GET['id']);
 	$poll_title = pg_escape_string($_POST['poll-title']);
 	$page_background = pg_escape_string($_POST['page_bg']);
-	$poll_background = pg_escape_string($_POST['poll_bg']);
-	$title_color = pg_escape_string($_POST['poll_title']);
+	$poll_background = pg_escape_string($_POST['poll_bg_color']);
+	$title_color = pg_escape_string($_POST['poll_title_color']);
+	$item_color = pg_escape_string($_POST['poll_item_color']);
+	$desc_color = pg_escape_string($_POST['poll_desc_color']);
+	$count_back_color = pg_escape_string($_POST['count_bg_color']);
+	$count_text_color = pg_escape_string($_POST['count_text_color']);
 	
 	// Poll Logo Upload and Show
 	$uploaddir = 'poll-logo/';
 	$uploadfile = $uploaddir . basename($_FILES['myimage']['name']);	
 	
-	if (move_uploaded_file($_FILES['myimage']['name'], $uploadfile))
+	if (move_uploaded_file($_FILES['myimage']['temp_name'], $uploadfile))
 	{    
 		//echo "File is valid, and was successfully uploaded.\n"; 
 	}
 	else   {   echo "File size greater than 300kb!\n\n";   }	
 
 	// Checking exiting Record 	
-	$select = 'SELECT * from poll_setting where poll_setting."poll_id" = \''.$poll_id.'\'';
+	$select = 'SELECT * from poll_setting where poll_setting."Poll_id" = \''.$poll_id.'\'';
 	$select_result = pg_query($select) or die('Query failed: ' . pg_last_error());
 	
 	$rows = pg_num_rows($select_result);
 	
 	if ($rows == 1) {
 		// Update Exiting Poll Logo
-		$update = 'UPDATE poll_setting SET "poll_id"= \''.$poll_id.'\', "logo_path"= \''.$uploadfile.'\', "poll_title"= \''.$poll_title.'\', "page_bg"= \''.$page_background.'\', "poll_bg"= \''.$poll_background.'\', "title_color"= \''.$title_color.'\' where poll_setting."poll_id" = \''.$poll_id.'\'';	
+		$update = 'UPDATE poll_setting SET "Poll_id"= \''.$poll_id.'\', "Logo_Path"= \''.$uploadfile.'\', "Poll_Title"= \''.$poll_title.'\', "Page_Bg_Color"= \''.$page_background.'\', "Poll_Bg_Color"= \''.$poll_background.'\', "Poll_Title_Color"= \''.$title_color.'\', "Poll_Item_Color"= \''.$item_color.'\', "Description_Color"= \''.$desc_color.'\', "Count_BG_Color"= \''.$count_back_color.'\', "Count_Text_Color"= \''.$count_text_color.'\' where poll_setting."Poll_id" = \''.$poll_id.'\'';	
 		
 		$update_result = pg_query($update) or die('Query failed: ' . pg_last_error());
 	
 	} 
 	else {
 		// Insert New Poll Logo
-		$insert = 'INSERT INTO poll_setting ("poll_id","logo_path","poll_title","page_bg","poll_bg",title_color) VALUES (\''.$poll_id.'\',\''.$uploadfile.'\',\''.$poll_title.'\',\''.$page_background.'\',\''.$poll_background.'\',\''.$title_color.'\')';
+		$insert = 'INSERT INTO poll_setting ("Poll_id","Logo_Path","Poll_Title","Page_Bg_Color","Poll_Bg_Color","Poll_Title_Color","Poll_Item_Color","Description_Color","Count_BG_Color","Count_Text_Color") VALUES (\''.$poll_id.'\',\''.$uploadfile.'\',\''.$poll_title.'\',\''.$page_background.'\',\''.$poll_background.'\',\''.$title_color.'\',\''.$item_color.'\',\''.$desc_color.'\',\''.$count_back_color.'\',\''.$count_text_color.'\')';
 		$insert_result = pg_query($insert) or die('Query failed: ' . pg_last_error());
 	}
 } 
@@ -44,18 +48,22 @@ if(isset($_GET['id']))
 {
 	$p_id= pg_escape_string($_GET['id']);
 	
-	$query = 'SELECT * from poll_setting where poll_setting."poll_id" = \''.$p_id.'\'';
+	$query = 'SELECT * from poll_setting where poll_setting."Poll_id" = \''.$p_id.'\'';
 	
 	$result = pg_query($query) or die('Query failed: ' . pg_last_error());	
 	
 	// output data of each row
 	while($row = pg_fetch_array($result)) {
-		$id= $row["poll_id"];
-		$a= $row["logo_path"];
-		$b= $row["poll_title"];
-		$c= $row["page_bg"];
-		$d= $row["poll_bg"];
-		$e= $row["title_color"];
+		$id= $row["Poll_id"];
+		$a= $row["Logo_Path"];
+		$b= $row["Poll_Title"];
+		$c= $row["Page_Bg_Color"];
+		$d= $row["Poll_Bg_Color"];
+		$e= $row["Poll_Title_Color"];
+		$f= $row["Poll_Item_Color"];
+		$g= $row["Description_Color"];
+		$h= $row["Count_BG_Color"];
+		$i= $row["Count_Text_Color"];
 	}
 }
 ?> 
@@ -108,14 +116,18 @@ include('left-sidebar.php');
 			<!-- Logo Upload -->
 			<div id="poll-settings">
 			<form method="POST" action="" enctype="multipart/form-data">
-				<p>Upload Logo : <input type="file" name="myimage"/></p>
+				<h3>Upload Logo : </h3><p><input type="file" name="myimage"/></p>
 					<div id="show-image">
 						<img src="http://app.upvoteapp.com/poll-admin/eliteadmin-horizontal-nav-fullwidth/<?php echo ($a); ?>" width="100" height="100">
 					</div>			
-				<p>Poll Title : <input id="" name="poll-title" type="text" placeholder="Poll Title" value="<?php echo($b); ?>"></p>
-				<p>Page Background Color : <input type="text" name="page_bg" class="colorpicker" value="<?php echo($c); ?>" /></p>	
-				<p>Poll Background Color : <input type="text" name="poll_bg" class="colorpicker" value="<?php echo($d); ?>" /></p>
-				<p>Poll Title Color : <input type="text" name="poll_title" class="colorpicker" value="<?php echo($e); ?>" /></p>	
+				<h3>Poll Title : </h3><p><input id="" name="poll-title" type="text" placeholder="Poll Title" value="<?php echo($b); ?>"></p>
+				<h3>Page Background Color : </h3><p><input type="text" name="page_bg" class="colorpicker" value="<?php echo($c); ?>" /></p>	
+				<h3>Poll Background Color : </h3><p><input type="text" name="poll_bg_color" class="colorpicker" value="<?php echo($d); ?>" /></p>
+				<h3>Poll Title Color : </h3><p><input type="text" name="poll_title_color" class="colorpicker" value="<?php echo($e); ?>" /></p>
+				<h3>Poll Item Color : </h3><p><input type="text" name="poll_item_color" class="colorpicker" value="<?php echo($f); ?>" /></p>
+				<h3>Poll Description Color : </h3><p><input type="text" name="poll_desc_color" class="colorpicker" value="<?php echo($g); ?>" /></p>	
+				<h3>Count BG Color : </h3><p><input type="text" name="count_bg_color" class="colorpicker" value="<?php echo($h); ?>" /></p>
+				<h3>Count Text Color : </h3><p><input type="text" name="count_text_color" class="colorpicker" value="<?php echo($i); ?>" /></p>
 				<input id="setting_submit" type="submit" name="submit" value="Save">
 			</form>
 			</div>
