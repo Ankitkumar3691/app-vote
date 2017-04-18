d08c7229e4121d34ecae8ab888a358e6c27cc5a0
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-19 01:13:09 +0530

    Update poll-setting.php

--- poll-admin/eliteadmin-horizontal-nav-fullwidth/poll-setting.php
+++ poll-admin/eliteadmin-horizontal-nav-fullwidth/poll-setting.php
@@ -106,7 +106,7 @@ if(isset($_GET['id']))
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
-    $(".bootstrap-switch-id-User_Request").click(function(){
+    $(".bootstrap-switch-container").click(function(){
         $("#request_options ul").toggle();
     });
 });
