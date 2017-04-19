4f2034991a4f9311da63c269297142cba079fc11
Ankitkumar3691 <ankitkumar3691@hotmail.com>
2017-04-20 03:13:58 +0530

    Update poll-with-settings.php

--- poll-with-settings.php
+++ poll-with-settings.php
@@ -60,7 +60,7 @@ $sg = new \SendGrid($apiKey);
 $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 echo $response->headers();
-//echo $response->body();
+echo $response->body();
 }
 ?>
 <!DOCTYPE html>
